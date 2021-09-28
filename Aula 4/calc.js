a9a6b92b9d05962df4114df8bfdcc4b918bc52f2
Gabriel Goulart <gabriel.vieira@saudegarantida.com.br>
2021-09-28 19:02:48 -0300

    Modulos

---
+++ Aula 4/calc.js
@@ -0,0 +1,14 @@
+
+var SomaFunc = require("./somar");
+var SubFunc = require("./sub");
+var DivFunc = require("./div");
+var MultFunc = require("./mult");
+
+
+console.log(SomaFunc(1,2));
+console.log(SubFunc(1,2));
+console.log(DivFunc(1,2));
+console.log(MultFunc(1,2));
+
+
+
