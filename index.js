cbb814297f704b42a93d81f47993eaad4158021f
Gabriel Goulart <gabriel.vieira@saudegarantida.com.br>
2021-09-30 19:43:26 -0300

    Requisições

--- index.js
+++ index.js
@@ -3,14 +3,18 @@ const app = express();
 
 app.get("/", function(req, res) {
     res.send("Seja bem-vindo")
-})
+});
 
 app.get("/sobre", function(req, res){
     res.send("Minha pagina sobre");
-})
+});
 
 app.get("/blog", function(req, res){
     res.send("Bem bindo ao meu blog!");
+});
+
+app.get('/ola/:cargo/:nome/:cor', function(req, res) {
+    res.send("<h1>Olá "+req.params.nome+"</h1>" + "<h2> O seu cargo é " + req.params.cargo + " E sua cor favorita é " + req.params.cor + "</h2>")
 })
 
 app.listen(8081, function() {
