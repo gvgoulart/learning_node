7cf9b5bbed97a51bdb6378690687e3bd276944b9
Gabriel Vieira <gabriel.goulart01@fatec.sp.gov.br>
2021-10-02 19:57:15 -0300

    Models no sequelize

--- teste.js
+++ teste.js
@@ -4,8 +4,40 @@ const sequelize = new Sequelize('test', 'root', '', {
     dialect: "mysql"
 });
 
-sequelize.authenticate().then(function(){
-    console.log("Conectado com sucesso!");
-}).catch(function() {
-    console.log("Falha ao se conectar: " + erro);
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 });
+
+Postagem.create({
+    titulo: "Um titulo qualquer",
+    conteudo: "conteudo teste aslkdmalkdkas"
+})
+
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type:Sequelize.STRING
+    }
+});
+
+Usuario.create({
+    nome: "Gabriel",
+    sobrenome: "Vieira",
+    idade: "21",
+    email: "teste@gabriel.com"
+})
+//Usuario.sync({force: true})
