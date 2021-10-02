3ef9f6899ac2dcfb8d3b63817d86adc9a9f20614
Gabriel Vieira <gabriel.goulart01@fatec.sp.gov.br>
2021-10-02 20:08:49 -0300

    Handlebars

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 const express = require("express");
 const app = express();
+const handlebars = require('express-handlebars');
+const Sequelize = require('sequelize');
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/html/index.html");
-});
 
-app.get("/sobre", function(req, res){
-    res.sendFile(__dirname + "/html/sobre.html");
-});
+// Config  
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Conexao com o banco de dados MySql
+        const sequelize = new Sequelize('test', 'root', '', {
+            host: "localhost",
+            dialect: "mysql"
+        });
 
-app.get("/blog", function(req, res){
-    res.send("Bem bindo ao meu blog!");
-});
-
-app.get('/ola/:cargo/:nome/:cor', function(req, res) {
-    res.send("<h1>Olá "+req.params.nome+"</h1>" + "<h2> O seu cargo é " + req.params.cargo + " E sua cor favorita é " + req.params.cor + "</h2>")
-})
 
 app.listen(8081, function() {
     console.log("servidor rodando na url tanto faz");
