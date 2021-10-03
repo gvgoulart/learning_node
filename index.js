124ce31f3c65b98a57e146f5757fb821a6303d7f
Gabriel Vieira <gabriel.goulart01@fatec.sp.gov.br>
2021-10-02 23:17:44 -0300

    adicionando dados pelo formulario e redirecionamento

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const Post = require('./models/Post');
 
 
 // Config  
@@ -11,15 +12,24 @@ const bodyParser = require('body-parser')
     //Body Parser
         app.use(bodyParser.urlencoded({extended: false}))
         app.use(bodyParser.json())
-
 // Rotas
+        app.get('/', function(req, res){
+            res.render('home')
+        })
 
         app.get('/cad', function(req, res){
             res.render('formulario')
         });
 
         app.post('/add', function(req, res){
-            res.send('Texto: '+req.body.titulo+"Conteudo: "+req.body.conteudo)
+            Post.create({
+                titulo: req.body.titulo,
+                conteudo: req.body.conteudo
+            }).then(function(){
+                res.redirect("/")
+            }).catch(function(erro){
+                res.send("Houve um erro: " + erro)
+            });
         });
 
 app.listen(8081, function() {
